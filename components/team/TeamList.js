0d0bf70f77f5051ba461b945337f8d296386beaa
Ali God <aligod8001@gmail.com>
2023-02-23 09:36:16 +0330

    [CHNG,RMV] change use linearSearch insted of quickSort && remove quickSort functions.

--- components/team/TeamList.js
+++ components/team/TeamList.js
@@ -20,7 +20,9 @@ const TeamList = (props) => {
   );
 
   if (props.teams.length > 0) {
-    const sortedTeam = quickSort(props.teams, 0, props.teams.length-1)
+    // const sortedTeam = quickSort(props.teams, 0, props.teams.length-1)
+    const sortedTeam = linearSearch(props.teams)
+    console.log(sortedTeam)
     output = sortedTeam.map((team) => (
       <TeamItem key={team.id} id={team.id} teamInfo={team} />
     ));
@@ -30,6 +32,29 @@ const TeamList = (props) => {
 };
 
 export default TeamList;
+
+const linearSearch = arr => {
+  const n = arr.length
+
+  for (let i = 0; i < n; i++) {
+    let m = i
+    for (let j = i+1; j < n-1; j++) {
+      if ( arr[j].point > arr[m].point ) {
+        m = j
+      } else if ( arr[j].point === arr[m].point ) {
+        if ( arr[j].difference > arr[m].difference ) {
+          m = j
+        }
+      }
+    }
+
+    if ( m !== i) {
+      swap(arr, i, m)
+    }
+  }
+
+  return arr
+}
 
 const swap = (arr, a, b) => {
   let temp = arr[a]
