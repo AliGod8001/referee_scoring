e2847b62df4b798085df9c435c81bc156c77f815
Ali God <aligod8001@gmail.com>
2023-02-23 09:37:38 +0330

    [ADD] add difference compare to sort.

--- components/team/TeamList.js
+++ components/team/TeamList.js
@@ -20,7 +20,6 @@ const TeamList = (props) => {
   );
 
   if (props.teams.length > 0) {
-    // const sortedTeam = quickSort(props.teams, 0, props.teams.length-1)
     const sortedTeam = linearSearch(props.teams)
     console.log(sortedTeam)
     output = sortedTeam.map((team) => (
@@ -60,29 +59,4 @@ const swap = (arr, a, b) => {
   let temp = arr[a]
   arr[a] = arr[b]
   arr[b] = temp
-}
-
-const partition = (arr, l, h) => {
-  const pivot = arr[h]
-  let i = l-1
-
-  for (let j = l; j <= h-1; j++) {
-    if ( arr[j].point > pivot.point ) {
-      i++;
-      swap(arr, i, j)
-    }
-  }
-
-  swap(arr, i+1, h)
-  return i+1
-}
-
-const quickSort = (arr, l, h) => {
-  if ( l < h ) {
-    const p = partition(arr, l, h)
-    quickSort(arr, l, p-1)
-    quickSort(arr, p+1, h)
-  }
-
-  return arr;
 }
